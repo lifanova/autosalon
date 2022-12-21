42c5da148c41787420ed8b1540abc9f2fce60883
vlifanova <vlifanova2@alfabank.ru>
2022-12-21 23:00:50 +0300

    Autosalon

--- src/main/java/Autosalon.java
+++ src/main/java/Autosalon.java
@@ -2,9 +2,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Autosalon {
+    public static final int MAX_VALUE = 10;
+    public static final int MILLIS = 3000;
     private List<Auto> auto = new ArrayList<>();
     private Customer customer = new Customer(this);
-    private int iMax = 10;
+    private static int iMax = MAX_VALUE;
 
 
     public void sellAuto() {
@@ -12,7 +14,7 @@ public class Autosalon {
 
         try {
             while (iMax > 0) {
-                Thread.sleep(3000);
+                Thread.sleep(MILLIS);
                 System.out.printf("%s подготовил автомобиль к продаже\n", Thread.currentThread().getName());
                 auto.add(new Auto());
                 customer.receiveAuto();
--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,4 +1,6 @@
 public class Customer {
+    public static final int MILLIS_FOR_BUY = 2000;
+    public static final int MILLIS_FOR_RECEIVE = 1000;
     private Autosalon autosalon;
 
     public Customer(Autosalon autosalon) {
@@ -8,7 +10,7 @@ public class Customer {
     public synchronized Auto buyAuto() {
         try {
             System.out.printf("%s зашёл в автосалон\n", Thread.currentThread().getName());
-            Thread.sleep(2000);
+            Thread.sleep(MILLIS_FOR_BUY);
             while (autosalon.getAuto().size() == 0) {
                 System.out.println("Нет автомобилей в наличии");
                 wait();
@@ -24,10 +26,18 @@ public class Customer {
 
     public synchronized void receiveAuto() {
         try {
-            Thread.sleep(1000);
+            Thread.sleep(MILLIS_FOR_RECEIVE);
             notify();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public Autosalon getAutosalon() {
+        return autosalon;
+    }
+
+    public void setAutosalon(Autosalon autosalon) {
+        this.autosalon = autosalon;
     }
 }
